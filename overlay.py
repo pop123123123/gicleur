539b94d2440e1d11cd394f870b699fb31fea7102
Louis MOUHAT <louis.mouhat@gmail.com>
2021-02-12 14:51:25 +0100

    add line breaking

--- overlay.py
+++ overlay.py
@@ -1,5 +1,7 @@
 from PIL import Image, ImageDraw, ImageFont
 
+from image_utils import ImageText
+
 MAX_SIZE = 1 << 8
 
 
@@ -15,29 +17,50 @@ def overlay(msg, in_path, out_path):
     img = img.resize(get_size(img.size))
     w, h = img.width, img.height
 
-    overlay = Image.new("RGBA", img.size, (255, 255, 255, 0))
-    overlayDraw = ImageDraw.Draw(overlay)
-
     r_height = max(h // 8, 26)
     h_mid = 2 * h // 3
-    shape = [(0, h_mid - r_height // 2), (w, h_mid + r_height // 2)]
-    overlayDraw.rectangle(shape, fill=(0, 0, 0, 128))
-    f_size = int(r_height * .6)
-    fnt = ImageFont.truetype("LiberationSans-Regular.ttf", f_size)
-    tw, th = overlayDraw.textsize(msg, font=fnt)
-    overlayDraw.text(
-        ((w - tw) / 2, h_mid - th // 2),
+    f_size = int(r_height * 0.6)
+
+    imgText = ImageText(img.size)
+    box_size = imgText.write_text_box(
+        (0, h_mid),
         msg,
-        font=fnt,
-        fill=(255, 255, 255, 200),
+        box_width=int(w * 0.95),
+        font_filename="LiberationSans-Regular.ttf",
+        font_size=f_size,
+        color=(255, 255, 255, 200),
+        place="center",
     )
 
+    imgText = ImageText(img.size)
+    imgText.write_text_box(
+        (0, h_mid - box_size[1] // 2),
+        msg,
+        box_width=int(w * 0.95),
+        font_filename="LiberationSans-Regular.ttf",
+        font_size=f_size,
+        color=(255, 255, 255, 200),
+        place="center",
+    )
+
+    r_height = box_size[1] + f_size * 0.6
+    shape = [(0, h_mid - r_height // 2), (w, h_mid + r_height // 2)]
+
+    overlay = Image.new("RGBA", img.size, (255, 255, 255, 0))
+    overlayDraw = ImageDraw.Draw(overlay)
+    overlayDraw.rectangle(shape, fill=(0, 0, 0, 128))
+
     out = Image.alpha_composite(img, overlay)
+    out = Image.alpha_composite(out, imgText.image)
     out.convert("RGB").save(out_path, "JPEG")
 
 
 if __name__ == "__main__":
+    import sys
+
     msg = "Envi de gicler"
+    if len(sys.argv) > 1:
+        msg = sys.argv[1]
     in_path = "test.png"
     out_path = "out.jpg"
     overlay(msg, in_path, out_path)
